72e1aa04d926071559dbdb4d680067e9886a9d90
kongfl888 K <kongfl888@outlook.com>
2020-08-21 14:06:58 +0800

    udate-adbyby:update

--- scripts/update-adbyby.sh
+++ scripts/update-adbyby.sh
@@ -4,6 +4,11 @@
 #
 # input adbyby dir which include sh files, eg: /usr/share/adbyby
 
+
+if [ ! -d ${1} ];then
+    exit 0
+fi
+
 for f in ${1}/*.sh; do
     sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/md5.json#https://raw.githubusercontent.com/kongfl888/ad-rules/master/md5.json#g' $f
     sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/lazy.txt#https://raw.githubusercontent.com/kongfl888/ad-rules/master/lazy.txt#g' $f
