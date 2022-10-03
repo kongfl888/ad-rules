1695cc76cc29bd845386f48581d8b8381094ecae
kongfl888 K <kongfl888@outlook.com>
2022-10-03 23:52:59 +0800

    use cdn for adbyby

--- scripts/patch-adbyby.sh
+++ scripts/patch-adbyby.sh
@@ -10,7 +10,7 @@ if [ ! -d ${1} ];then
 fi
 
 for f in ${1}/*.sh; do
-    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/md5.json#https://raw.githubusercontent.com/kongfl888/ad-rules/master/md5.json#g' $f
-    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/lazy.txt#https://raw.githubusercontent.com/kongfl888/ad-rules/master/lazy.txt#g' $f
-    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/video.txt#https://raw.githubusercontent.com/kongfl888/ad-rules/master/video.txt#g' $f
+    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/md5.json#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/md5.json#g' $f
+    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/lazy.txt#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/lazy.txt#g' $f
+    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/video.txt#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/video.txt#g' $f
 done
