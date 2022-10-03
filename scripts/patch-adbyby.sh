499e99b401ff494524922bbb663bf9e3e1a9816a
kongfl888 K <kongfl888@outlook.com>
2022-10-03 23:58:51 +0800

    update link

--- scripts/patch-adbyby.sh
+++ scripts/patch-adbyby.sh
@@ -10,7 +10,7 @@ if [ ! -d ${1} ];then
 fi
 
 for f in ${1}/*.sh; do
-    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/md5.json#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/md5.json#g' $f
-    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/lazy.txt#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/lazy.txt#g' $f
-    sed -i 's#https://adbyby.coding.net/p/xwhyc-rules/d/xwhyc-rules/git/raw/master/video.txt#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/video.txt#g' $f
+    sed -i -r 's#https://.*/md5.json#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/md5.json#g' $f
+    sed -i -r 's#https://.*/lazy.txt#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/lazy.txt#g' $f
+    sed -i -r 's#https://.*/video.txt#https://cdn.jsdelivr.net/gh/kongfl888/ad-rules/video.txt#g' $f
 done
